1b1821e3b6d83169554154a5ff0c379f1504a7af
JoshuaOndieki <joshuaondieki8@gmail.com>
2017-04-03 21:57:51 +0300

    add test cases for prime numbers generator

---
+++ Day 1/prime_numbers/TestCases.py
@@ -0,0 +1,23 @@
+#prime numbers function test cases
+import unittest
+from .prime import prime_numbers
+
+class PrimeTest(unittest.TestCase):
+    def test_returns_prime_numbers(self):
+        self.assertListEqual(prime_numbers(6), [3,5], msg="Range of 0-6 should return [3,5] as the prime numbers")
+
+    def test_input_is_a_number(self):
+        with self.assertRaises(TypeError, msg="Should raise type error if a string is passed as argument"):
+            prime_numbers("String")
+
+    def test_return_value_is_list(self):
+        self.assertTrue(isinstance(prime_numbers(10), list), msg="The function should return a list")
+
+    def test_returns_0_for_negative_input(self):
+        self.assertEquals(prime_numbers(-30),0,msg="Should return zero since there are no negative primes")
+
+    def test_does_not_return_negative_primes(self):
+        self.assertGreater(min(prime_numbers(50)),0)
+
+    def test_does_not_include_non_primes(self):
+        self.assertNotIn([0,1,2,4,6],prime_numbers(6))
