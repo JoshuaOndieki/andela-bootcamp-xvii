367a24a5d35c609854a651787ea0977c5dc9a99d
JoshuaOndieki <joshuaondieki8@gmail.com>
2017-04-05 10:57:15 +0300

    Make is_prime function more efficient

--- Day 1/prime_numbers/prime.py
+++ Day 1/prime_numbers/prime.py
@@ -1,22 +1,27 @@
 #prime numbers generator
 
+from math import sqrt
+
 def prime_numbers(upper_limit):
     prime_list=[]
-    def is_prime(n):
-        if n in [0,1,2]:
+    def is_prime(n):  # prime checker
+        sqrt_=sqrt(n)
+        if n < 2:
             return False
-        for i in range(2, n):
-            if n % i == 0:
+        elif n==2:
+            return True
+        for i in range(2,n):
+            if n%i==0:
                 return False
+            if sqrt_<i: #minimize the numbers to be checked for efficiency
+                break
         return True
     try:
         if upper_limit<0:
-            return 0
+            return "No prime numbers within that range! All prime numbers are positive"
         for number in range(upper_limit+1):
             if is_prime(number):
                 prime_list.append(number)
         return prime_list
     except TypeError:
         raise TypeError
-
-
